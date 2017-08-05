64486e3b3e7a3b535326b29039d0cd90575fb4b7
ranjitha97 <30342436+ranjitha97@users.noreply.github.com>
2017-08-05 16:36:46 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    double a, b, c;
+    printf("Enter three different numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if( a>=b && a>=c )
+        printf("%d is the largest number.", a);
+    if( n2>=n1 && n2>=n3 )
+        printf("%d is the largest number.", b);
+    if( n3>=n1 && n3>=n2 )
+        printf("%.d is the largest number.", c);
+    return 0;
+}
