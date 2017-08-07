7cba0d8828b94a33c1d3a68ef613e19d537cac39
ranjitha97 <30342436+ranjitha97@users.noreply.github.com>
2017-08-07 17:51:44 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int low, high, y, flag;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &low, &high);
+
+    printf("Prime numbers between %d and %d are: ", low, high);
+
+    while (low < high)
+    {
+        flag = 0;
+
+        for(y= 2; y<= low/2; ++y)
+        {
+            if(low % y == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", low);
+
+        ++low;
+    }
+
+    return 0;
+}
