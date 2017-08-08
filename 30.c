6252c4d206d3228c39210722e1d10997ab5d5945
ranjitha97 <30342436+ranjitha97@users.noreply.github.com>
2017-08-08 12:11:53 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int i, n=15, sum=0;
+    
+    printf("Enter upper limit: ");
+    scanf("%d", &n);
+void num()
+    {
+    for(i=1; i<=n; i++)
+    {
+        sum += i;
+    }
+
+    printf("Sum of first %d natural numbers = %d", n, sum);
+}
+void odd()
+int a=45;i;sum=0;
+{
+for(i=15;i<=a;i++)
+if(n%2!=0)
+{
+sum+=i;
+}
+printf("sum of odd number=%d"sum);
+}
+raturn 0
+}
