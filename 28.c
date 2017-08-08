a7f302b31c41943946fe6260672cdb736906d051
ranjitha97 <30342436+ranjitha97@users.noreply.github.com>
2017-08-08 11:27:35 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     bool Ans = true;
+     while (Ans)
+    {
+          if(THE BUTTON Q IS PRESSED ON KEYBOARD)
+          break;
+    }
+}
